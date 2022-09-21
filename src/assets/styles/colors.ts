d76ccf0f2c22067f46c89951191f7665ff61b5ae
Keven Ferreira Moraes <55964670+Kevenfz@users.noreply.github.com>
2022-09-20 23:11:23 -0300

    style"Header": Estilização completa

--- src/assets/styles/colors.ts
+++ src/assets/styles/colors.ts
@@ -1,8 +1,11 @@
 export const colors = {
   headerColor: "#ffff",
   hoverHeaderColor: "#04bf55",
+  backgroundSearchFocusColor: "#009440b7",
   searchColor: "#C2F2DC",
   backgroundCard: "#C8E6D2",
   buttonColor: "#04BF55",
+  buttonSearch: "#C2F2DC",
   footerColor: "#014034",
+  textLogoColor: "#014034",
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,35 @@
-import React from "react";
+import { CgBorderStyleSolid } from "react-icons/cg";
 import LogoNature from "../../assets/Icons/nature_future_ticket_1.png";
-import "../../app.css";
+import "../../fonts/Intro-Rust/stylesheet.css";
+import "./style.css";
 import * as S from "./style";
 
 export const Home = () => {
   return (
     <>
       <S.Header>
-        <S.logoSearchHeader>
+        <S.LogoSearchHeader>
           <S.LogoHeader src={LogoNature} alt="Logo Nature Future" />
-          <S.logoNameHeader className="font-face-header-span">
-            NATURE FUTURE TICKET
-          </S.logoNameHeader>
+          <S.LogoNameHeader>NATURE FUTURE TICKET</S.LogoNameHeader>
 
-          <input type="search" placeholder="Pesquisar" />
-          <button type="button">Procurar</button>
+          <S.SearchDiv>
+            <S.Search>
+              <S.InputSearch type="search" placeholder="Pesquisar" />
+              <CgBorderStyleSolid className="between" />
+              <S.BtnSearch type="button">Buscar</S.BtnSearch>
+            </S.Search>
+          </S.SearchDiv>
 
-          <p>Home</p>
-          <p>Sobre nós</p>
-          <p>Cadastre-se</p>
-          <p>Login</p>
-        </S.logoSearchHeader>
+          <S.OptionsHeader>
+            <S.TitleOptionsHeader>Home</S.TitleOptionsHeader>
+            <S.TitleOptionsHeader>Sobre nós</S.TitleOptionsHeader>
+            <S.CadLogin>
+              <S.TitleOptionsHeader>Login</S.TitleOptionsHeader>
+              <CgBorderStyleSolid className="betweenCadLogin" />
+              <S.TitleOptionsHeader>Cadastre-se</S.TitleOptionsHeader>
+            </S.CadLogin>
+          </S.OptionsHeader>
+        </S.LogoSearchHeader>
       </S.Header>
     </>
   );
