a3daa66cee0993f39841d0b207a66c04496cd491
Keven Ferreira Moraes <55964670+Kevenfz@users.noreply.github.com>
2022-11-03 22:35:09 -0300

    feat"RecoverPasswordPage': Pagina de recuperação de senha criada e funcional

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,16 @@
-import { createUserType } from "../types/types";
+import { createUserType, RecoverEmailType } from "../types/types";
 import api from "./api";
 
 const userService = {
   createUser: (values: createUserType) =>
     api
       .post("users/create", values)
+      .then((response: any) => response)
+      .catch((error: any) => error.response),
+
+  recoverEmail: (email: string) =>
+    api
+      .patch(`users/recovery/${email}`)
       .then((response: any) => response)
       .catch((error: any) => error.response),
 };
