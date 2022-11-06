f7b54f143b62b514b959e91bf00b2cf4ec78c502
Keven Ferreira Moraes <55964670+Kevenfz@users.noreply.github.com>
2022-11-06 02:38:46 -0300

    style"Footer": Refatoração do icone e background

--- src/assets/styles/mixins.ts
+++ src/assets/styles/mixins.ts
@@ -340,7 +340,9 @@ export const TitleLogoFooter = () => css`
 const Footer = () => css`
   width: 100vw;
   height: 220px;
-  background-color: ${colors.footerColor};
+  /* background-color: ${colors.footerColor}; */
+  background-color: #014034;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 48 48'%3E%3Cg fill='%23000000' fill-opacity='0.03'%3E%3Cpath d='M12 0h18v6h6v6h6v18h-6v6h-6v6H12v-6H6v-6H0V12h6V6h6V0zm12 6h-6v6h-6v6H6v6h6v6h6v6h6v-6h6v-6h6v-6h-6v-6h-6V6zm-6 12h6v6h-6v-6zm24 24h6v6h-6v-6z'%3E%3C/path%3E%3C/g%3E%3C/svg%3E");
   display: flex;
   grid-template-column: auto auto auto auto auto;
   justify-content: center;
